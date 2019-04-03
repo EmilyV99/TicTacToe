f45b9a92b9e1b366b65a1104f84960b501ab12b1
Brian <Bjredding56@gmail.com>
2019-04-02 20:20:46 -0400

    AI: Test commit

--- src/TTTComputerPlayer.java
+++ src/TTTComputerPlayer.java
@@ -13,17 +13,18 @@ public class TTTComputerPlayer
 
     public void takeTurn()
     {
-        //Take the AI's turn, using the `board` object.
-        //If you need a copy of the board to modify for some reason, you can do:
-        //TTTBoard tempBoard = board.cloneBoard();
-        //This will give you a copy of the current state of the board.
-        //`ID` is 1 or 2 (X or O); the one the AI is using
-        //`opponentID` is 1 or 2 (X or O); the one the human is using
-        //You can make whatever helper methods you need to accomplish this task. You can even make other classes, if you think it would help.
-        //The only condition is that it all needs to run from THIS method, as though this were `main()`
-        //-V
+		// MAKE A TURN LEFT AI 
     }
-
+	//Take the AI's turn, using the `board` object.
+	//If you need a copy of the board to modify for some reason, you can do:
+	//TTTBoard tempBoard = board.cloneBoard();
+	//This will give you a copy of the current state of the board.
+	//`ID` is 1 or 2 (X or O); the one the AI is using
+	//`opponentID` is 1 or 2 (X or O); the one the human is using
+	//You can make whatever helper methods you need to accomplish this task. You can even make other classes, if you think it would help.
+	//The only condition is that it all needs to run from THIS method, as though this were `main()`
+	//-V
+		
     //Driver method. This will not exist in the final form of the project.
     //Running this will pit 2 AIs against each other, since there is no coded human input at this time.
     //You can modify this however you like, this is just an example test.
