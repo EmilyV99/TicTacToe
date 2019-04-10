11d47b06c8fe3447037da7f429d612c6a7489fdc
Robert Venezia <rvenezia@cs.hartford.edu>
2019-04-10 16:19:21 -0400

    AI: Finish takeTurn() (HOPEFULLY!)

--- src/TTTBoardAnalyzer.java
+++ src/TTTBoardAnalyzer.java
@@ -1,90 +1,110 @@
 public class TTTBoardAnalyzer
 {
-    /**
-     * Values for certain row combinations.
-     */
-    private static final int
-            OOO = 1000, //Victory
-            OOB = 10, //2, and a blank
-            OOX = -5, //2, and one of the other
-            OBB = 4, //One, and 2 blanks
-            OBX = 0, //One of each
-            BBB = 0; //Blank
+    private static final int INVALID = Integer.MAX_VALUE,
+            XVAL = -1, TIEVAL = 0, OVAL = 1;
     
-    /**
-     * Scans a TTTBoard, and analyzes the position of each player.
-     * 
-     * @param board     The board to analyze
-     * @return          An integer value representing the advantage. Negative is good for 'X', positive good for 'O'.
-     */
-    public static int boardHeuristic(TTTBoard board)
+    public static int[] analyzeBoard(TTTBoard board, int ID)
     {
-        int winner = board.checkWinner();
-        if(winner != 0)
-        {
-            switch(winner)
-            {
-                case TTTBoard.X:
-                    return -OOO;
-                    
-                case TTTBoard.O:
-                    return OOO;
-                    
-                default:
-                    return BBB;
-            }
-        }
-        int totalValue = 0;
-        //Rows
-        totalValue += scanOneRow(board.getSpace(0, 0), board.getSpace(1, 0), board.getSpace(2, 0));
-        totalValue += scanOneRow(board.getSpace(0, 1), board.getSpace(1, 1), board.getSpace(2, 1));
-        totalValue += scanOneRow(board.getSpace(0, 2), board.getSpace(1, 2), board.getSpace(2, 2));
-        //Columns
-        totalValue += scanOneRow(board.getSpace(0, 0), board.getSpace(0, 1), board.getSpace(0, 2));
-        totalValue += scanOneRow(board.getSpace(1, 0), board.getSpace(1, 1), board.getSpace(1, 2));
-        totalValue += scanOneRow(board.getSpace(2, 0), board.getSpace(2, 1), board.getSpace(2, 2));
-        //Diagonals
-        totalValue += scanOneRow(board.getSpace(0, 0), board.getSpace(1, 1), board.getSpace(2, 2));
-        totalValue += scanOneRow(board.getSpace(2, 0), board.getSpace(1, 1), board.getSpace(0, 2));
-        return totalValue;
+        return analyzeBoard(board, ID, INVALID, INVALID);
     }
     
-    /**
-     * Scans a single row of the board.
-     * 
-     * @param a     First letter
-     * @param b     Second letter
-     * @param c     Third letter
-     * @return      An integer value representing the advantage. Negative is good for 'X', positive is good for 'O'
-     */
-    private static int scanOneRow(int a, int b, int c)
+    private static int[] analyzeBoard(TTTBoard board, int ID, int min, int max)
     {
-        //Count the spaces of each type. Position doesn't matter, as swapped positions within a single row have the same value. -V
-        int xcount = (a == TTTBoard.X ? 1 : 0) + (b == TTTBoard.X ? 1 : 0) + (c == TTTBoard.X ? 1 : 0);
-        int ocount = (a == TTTBoard.O ? 1 : 0) + (b == TTTBoard.O ? 1 : 0) + (c == TTTBoard.O ? 1 : 0);
-        int bcount = 3 - (xcount + ocount);
-        //All 3 are the same
-        if(xcount == 3) return -OOO;
-        if(ocount == 3) return OOO;
-        if(bcount == 3) return BBB;
-        //
-        if(bcount == 1)
-        {
-            //2 are the same, the third is blank
-            if(xcount == 2) return -OOB;
-            if(ocount == 2) return OOB;
-            //All 3 are different
-            return OBX; //Equal position for both sides, so net 0
-        }
-        //1 and 2 blanks
-        if(bcount == 2)
-        {
-            if(xcount == 1) return -OBB;
-            else return OBB;
-        }
-        //2 of one, blocked by the other
-        if(xcount == 2 && ocount == 1) return -OOX;
-        if(!(ocount == 2 && xcount == 1)) throw new RuntimeException("Whoops! Unhandled case! (" + a + " " + b + " " + c + ")");
-        return -OOX;
+        boolean minimize = ID == TTTBoard.X;
+        int[][] heuristics = new int[3][3];
+        boolean pruned = false;
+        for(int x = 0; x < 3; ++x)
+            for(int y = 0; y < 3; ++y)
+            {
+                if(board.getSpace(x,y) != TTTBoard.BLANK || pruned)
+                {
+                    heuristics[x][y] = INVALID;
+                    continue;
+                }
+                boolean prune = min != INVALID || max != INVALID;
+                TTTBoard temp = board.cloneBoard();
+                temp.setSpace(x, y, ID);
+                switch(temp.checkWinner())
+                {
+                    case TTTBoard.TIE:
+                        if(prune)
+                        {
+                            if(min != INVALID && max != INVALID)
+                            {
+                                if(minimize ? TIEVAL > max : TIEVAL < min)
+                                    pruned = true;
+                                else
+                                    if(minimize) max = TIEVAL;
+                                    else min = TIEVAL;
+                            }
+                        }
+                        heuristics[x][y] = TIEVAL;
+                        break;
+                    case TTTBoard.X:
+                        if(prune)
+                        {
+                            if(min != INVALID && max != INVALID)
+                            {
+                                if(minimize ? XVAL > max : XVAL < min)
+                                    pruned = true;
+                                else
+                                    if(minimize) max = XVAL;
+                                    else min = XVAL;
+                            }
+                        }
+                        heuristics[x][y] = XVAL;
+                        break;
+                    case TTTBoard.O:
+                        if(prune)
+                        {
+                            if(min != INVALID && max != INVALID)
+                            {
+                                if(minimize ? OVAL > max : OVAL < min)
+                                    pruned = true;
+                                else
+                                    if(minimize) max = OVAL;
+                                    else min = OVAL;
+                            }
+                        }
+                        heuristics[x][y] = OVAL;
+                        break;
+                    case TTTBoard.BLANK:
+                        heuristics[x][y] = analyzeBoard(temp, TTTBoard.oppositeID(ID), minimize ? INVALID : min, minimize ? max : INVALID)[2];
+                        if(prune)
+                        {
+                            if(min != INVALID && max != INVALID)
+                            {
+                                if(minimize ? heuristics[x][y] > max : heuristics[x][y] < min)
+                                    pruned = true;
+                                else
+                                    if(minimize) max = heuristics[x][y];
+                                    else min = heuristics[x][y];
+                            }
+                        }
+                        break;
+                }
+            }
+        
+        int[] data = new int[3];
+        data[3] = INVALID;
+        for(int x = 0; x < 3; ++x)
+            for(int y = 0; y < 3; ++y)
+            {
+                if(heuristics[x][y] == INVALID) continue;
+                if(data[2] == INVALID)
+                {
+                    data[0] = x;
+                    data[1] = y;
+                    data[2] = heuristics[x][y];
+                }
+                else if(minimize ? heuristics[x][y] < data[2] : heuristics[x][y] > data[2])
+                {
+                    data[0] = x;
+                    data[1] = y;
+                    data[2] = heuristics[x][y];
+                }
+            }
+        
+        return data;
     }
 }
--- src/TTTComputerPlayer.java
+++ src/TTTComputerPlayer.java
@@ -13,82 +13,9 @@ public class TTTComputerPlayer
 
     public void takeTurn()
     {
-        boolean minimize = ID == TTTBoard.X;
-        int[][] heuristics = new int[3][3];
-        int bestSoFar = Integer.MAX_VALUE;
-        for(int x = 0; x < 3; ++x)
-            for(int y = 0; y < 3; ++y)
-            {
-                if(board.getSpace(x,y) == TTTBoard.BLANK) continue;
-                TTTBoard temp = board.cloneBoard();
-                temp.setSpace(x, y, ID);
-                //Alpha-beta pruning needs to go here?
-                heuristics[x][y] = analyzeBoard(temp, TTTBoard.oppositeID(ID));
-                if(bestSoFar == Integer.MAX_VALUE) bestSoFar = heuristics[x][y];
-                else
-                {
-                    if(minimize)
-                        if(bestSoFar < heuristics[x][y])
-                }
-            }
-        int best = 0, bestx = -1, besty = -1;
-        for(int x = 0; x < 3; ++x)
-            for(int y = 0; y < 3; ++y)
-            {
-                if(board.getSpace(x,y) == TTTBoard.BLANK) continue;
-                if(heuristics[x][y] == Integer.MAX_VALUE) continue; //Alpha-beta pruned spot
-                if(bestx == -1) //Start at the first blank space, in case there is only 1!
-                {
-                    best = heuristics[x][y];
-                    bestx = x;
-                    besty = y;
-                    continue;
-                }
-                if(minimize)
-                {
-                    if(heuristics[x][y] < best)
-                    {
-                        best = heuristics[x][y];
-                        bestx = x;
-                        besty = y;
-                    }
-                }
-                else
-                {
-                    if(heuristics[x][y] > best)
-                    {
-                        best = heuristics[x][y];
-                        bestx = x;
-                        besty = y;
-                    }
-                }
-            }
-        board.setSpace(bestx, besty, ID);
+        int[] data = TTTBoardAnalyzer.analyzeBoard(board, ID);
+        board.setSpace(data[0], data[1], ID);
     }
-    
-    /* This is wrong and needs to be totally redone. Need to re-evaluate how I do this. -V
-    private int analyzeBoard(TTTBoard board, int ID)
-    {
-        //Starts just after the AI has moved, calculating the best move for the opponent. Returns a heuristic total for this board state, to the end of the game. 
-        boolean minimize = ID == TTTBoard.X;
-        int[][] heuristics = new int[3][3];
-        for(int x = 0; x < 3; ++x)
-            for(int y = 0; y < 3; ++y)
-            {
-                if(board.getSpace(x,y) == TTTBoard.BLANK) continue;
-                TTTBoard temp = board.cloneBoard();
-                temp.setSpace(x, y, ID);
-                //Alpha-beta pruning needs to go here?
-                heuristics[x][y] = analyzeBoard(temp, TTTBoard.oppositeID(ID));
-            }
-        
-    }
-    
-    private int analyzeBoard(TTTBoard board, int ID, int max, int min)
-    {
-        boolean minimize = ID == TTTBoard.X;
-         
-    }*/
 		
     //Driver method. This will not exist in the final form of the project.
     //Running this will pit 2 AIs against each other, since there is no coded human input at this time.
